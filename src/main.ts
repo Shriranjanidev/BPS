722afd44f4d51ca150c74f58cf5051b6e86effd7
= <=>
2018-11-14 19:03:44 +0530

    initial UI

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import '../src/vendor/jquery/jquery.min.js';
+import '../src/vendor/bootstrap/js/bootstrap.bundle.min.js';
+
+
+import '../src/vendor/jquery-easing/jquery.easing.min.js';
+import '../src/vendor/scrollreveal/scrollreveal.min.js';
+import '../src/vendor/magnific-popup/jquery.magnific-popup.min.js';
+
+
+
 if (environment.production) {
   enableProdMode();
 }
